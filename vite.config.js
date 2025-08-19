36dda6e4e25854dfe869df4426fd32afe6e3c0db
Joshua Magnase <68670239+joshua0006@users.noreply.github.com>
2025-08-19 09:49:20 +0800

    fix routing

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.gltf'],
+  server: {
+    historyApiFallback: true,
+  },
 })
